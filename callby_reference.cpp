4c9a0c27f0de7b8b5fda38bf2a99b5a1a52ad588
SparshS01 <srivastavasparsh088@gmail.com>
2025-08-28 21:28:31 +0530

    Create callby_reference.cpp

---
+++ callby_reference.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void swap( int*x, int* y)
+{
+    int swap;
+    swap=*x;
+    *x=*y;
+    *y=swap;
+}
+int main() {
+    int x=500, y=100;
+    swap(&x, &y);
+    cout<<"Value of x is:"<<x<<endl;
+    cout<<"Value of y is:"<<y<<endl;
+
+    return 0;
+
+}
+/* OUTPUT
+Value of x is:100
+Value of y is:500
+    */
