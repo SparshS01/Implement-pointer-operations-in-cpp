3ef131eb7e495191514d3bf3380f49d6fb3d2a3a
SparshS01 <srivastavasparsh088@gmail.com>
2025-08-28 21:30:40 +0530

    Create callbyreferencewithincrement.cpp

---
+++ callbyreferencewithincrement.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void Increment(int &s)
+{
+    s=s+5000;
+    cout<<"Salary inside function: "<<s<<endl;
+}
+int main()
+{
+    int sal=27000;
+    Increment(sal);
+    cout<<"Salary inside main: "<<sal<<endl;
+    return 0;
+
+}
+/*OUTPUT 
+Salary inside function: 32000
+Salary inside main: 32000
+    */
